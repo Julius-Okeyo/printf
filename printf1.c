2c081e8f890ed56166f18340d6ba1d53b7ec5e54
Julius-Okeyo <jaykopiyo@gmail.com>
2022-11-27 08:20:27 -0800

    Broke down code into manageable functions

---
+++ printf1.c
@@ -0,0 +1,180 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+
+int print_char(char c);
+int print_int(int rem);
+int print_string(char *s);
+/**
+ * print_char - prints a char
+ * @c: character to be printed
+ * Return: The number of characters printed
+ */
+
+int print_char(char c)
+{
+	int a = 0;
+
+	if (c != 0)
+	{
+		_putchar(c);
+		a++;
+	}
+	if (c == 0)
+	{
+		_putchar(' ');
+		a++;
+	}
+	return (a);
+}
+
+/**
+ * print_string - prints a string
+ * @str: string to be printed
+ * Return: number of characters printed
+ */
+
+int print_string(char *str)
+{
+	int a = 0;
+	char *emp = "";
+
+	if (str == emp)
+		return (0);
+	if (str == NULL)
+		str = "(null)";
+	while (*str)
+	{
+		_putchar(*str);
+		a++;
+		str++;
+	}
+	return (a);
+}
+
+/**
+ * print_int - prints a integer
+ * rem: integer to be printed
+ * Return: The number of characters printed
+ */
+
+int print_int(int rem)
+{
+	int num, k = 1, div = 10, sign = 0, a = 0;
+	
+	if (rem < 0)
+	{
+		sign = 1;
+		rem = (-1 * rem);
+	}
+	num = rem;
+	while (div >= 10)
+	{
+		k *= 10;
+		div = num / k;
+	}
+	if (sign == 1)
+	{
+		_putchar('-');
+		a++;
+	}
+	while (rem > 10)
+	{
+		div = rem / k;
+		rem = rem % k;
+		_putchar(div + '0');
+		a++;
+		k /= 10;
+	}
+	_putchar(rem + '0');
+	a++;
+	return (a);
+}
+
+
+/**
+ * _printf - mimics the behavior of printf
+ * @format: string to be printed including string formatters
+ * Return: The number of characters printed
+ */
+
+int _printf(const char *const format, ...)
+{
+	int a = 0, j = 0, i, z, rem;
+	char c;
+	char *str;
+	char *emp = "";
+	va_list arg;
+
+	if (format == NULL)
+		return (-1);
+	if (format == emp)
+		return (0);
+
+	va_start(arg, format);
+
+	while (format[j] != '\n' && format[j] != '\0')
+	{
+		if (format[j] == '%')
+		{
+			if (format[j + 1] == 'c' || format[j + 1] == 'C')
+			{
+				c = va_arg(arg, int);
+				a += print_char(c);
+			}
+			else
+			{
+				if (format[j + 1] == 's')
+				{
+					str = va_arg(arg, char *);
+					a += print_string(str);
+				}
+			       else
+			       {
+				       if (format[j + 1] == 'i' || format[j + 1] == 'd')
+				       {
+						rem = va_arg(arg, int);
+						a += print_int(rem);
+				       }
+				       else
+				       {
+					       if (format[j + 1] >= 48 && format[j + 1] <= 57)
+					       {
+						       z = (int)(format[j + 1] - 48);
+						       for (i = 0; i < z; i++)
+						       {
+							       _putchar(' ');
+							       a++;
+						       }
+						       j++;
+						       if (format[j + 1] == 'i' || format[j + 1] == 'd')
+						       {
+							       rem = va_arg(arg, int);
+							       a += print_int(rem);
+						       }
+					       }
+					       else
+					       {
+						       _putchar(format[j]);
+						       _putchar(format[j + 1]);
+						       a += 2;
+					       }
+				       }
+			       }
+			}
+			j++;
+		}
+		else
+		{
+			_putchar(format[j]);
+			a++;
+		}
+		j++;
+	}
+	_putchar('\n');
+	a++;
+	va_end(arg);
+	return (a);
+}
